07e598d095eacb4733d927fd4ba08649ac5af7ac
SUN <15144639564@163.com>
2022-03-09 10:11:53 +0800

    增加spring cloud注解

--- src/main/java/com/wolf/provider/ProviderApplication.java
+++ src/main/java/com/wolf/provider/ProviderApplication.java
@@ -2,8 +2,10 @@ package com.wolf.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProviderApplication {
 
 	public static void main(String[] args) {
